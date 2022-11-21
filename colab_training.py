e67e31316cb1a915416146bafb2925e08dd5a90c
Aditya Kannan <akannan2@andrew.cmu.edu>
2022-11-20 19:14:09 -0500

    added cpu training

--- colab_training.py
+++ colab_training.py
@@ -11,41 +11,76 @@ sys.path = [os.path.join(_ROOT, "/scripts")] + sys.path
 from desired_outputs import desired_outputs
 from importlib import reload
 from codecarbon import EmissionsTracker
-from opt_helper import save, load, get_training_curve, plot_training_curve, plot_result
+from opt_helper import save, load, plot_training_curve, plot_result
 
-import train_with_warp_drive as gpu_trainer
 
 tracker = EmissionsTracker()
 tracker.start()
 
+device = 'cpu' # 'gpu'
+
+if device == 'gpu':
+    import train_with_warp_drive as gpu_trainer
+elif device == 'cpu':
+    os.chdir(_ROOT+"/scripts")
+    import train_with_rllib as cpu_trainer
+
+
 print('Beginning no negotiation...')
-gpu_trainer_off, gpu_nego_off_ts = gpu_trainer.trainer(
-        negotiation_on=0, # no negotiation
-        num_envs=100, 
-        train_batch_size=1024, 
-        num_episodes=3000, 
-        lr=0.0005,
-        model_params_save_freq=5000, 
-        desired_outputs=desired_outputs, # a list of values that the simulator will output
-        output_all_envs=False # output the mean of all "num_envs" results. Set to True for output all results
-    )
+if device == 'gpu':
+    trainer_off, nego_off_ts = gpu_trainer.trainer(
+            negotiation_on=0, # no negotiation
+            num_envs=100, 
+            train_batch_size=1024, 
+            num_episodes=3000, 
+            lr=0.0005,
+            model_params_save_freq=5000, 
+            desired_outputs=desired_outputs, # a list of values that the simulator will output
+            output_all_envs=False # output the mean of all "num_envs" results. Set to True for output all results
+        )
+
+elif device == 'cpu':
+    cpu_trainer = reload(cpu_trainer)
+    trainer_off, nego_off_ts = cpu_trainer.trainer(negotiation_on=0,  # no negotiation
+            num_envs=1, 
+            train_batch_size=1024, 
+            num_episodes=300, 
+            lr=0.0005, 
+            model_params_save_freq=5000, 
+            desired_outputs=desired_outputs, # a list of values that the simulator will output
+            num_workers=1
+        )
 
 print('Beginning negotiation...')
-gpu_trainer_on, gpu_nego_on_ts = gpu_trainer.trainer(
-        negotiation_on=1, # no negotiation
-        num_envs=100, 
+if device == 'gpu':
+    trainer_on, nego_on_ts = gpu_trainer.trainer(
+            negotiation_on=1, # no negotiation
+            num_envs=100, 
+            train_batch_size=1024, 
+            num_episodes=3000, 
+            lr=0.0005,
+            model_params_save_freq=5000, 
+            desired_outputs=desired_outputs, # a list of values that the simulator will output
+            output_all_envs=False # output the mean of all "num_envs" results. Set to True for output all results
+        )
+
+elif device == 'cpu':
+    cpu_trainer = reload(cpu_trainer)
+    trainer_on, nego_on_ts = cpu_trainer.trainer(negotiation_on=1, # with naive negotiation
+        num_envs=1, 
         train_batch_size=1024, 
-        num_episodes=3000, 
-        lr=0.0005,
+        num_episodes=300, 
+        lr=0.0005, 
         model_params_save_freq=5000, 
         desired_outputs=desired_outputs, # a list of values that the simulator will output
-        output_all_envs=False # output the mean of all "num_envs" results. Set to True for output all results
+        num_workers=1
     )
+
 
 # saving and loading
 save_path = 'results.pkl'
 print(f'Saving results to {save_path}')
-save({"nego_off":gpu_nego_off_ts, "nego_on":gpu_nego_on_ts}, save_path)
+save({"nego_off":nego_off_ts, "nego_on":nego_on_ts}, save_path)
 # dict_ts = load("filename.pkl")
 # nego_off_ts, nego_on_ts = dict_ts["nego_off"], dict_ts["nego_on"]
 
@@ -59,8 +94,8 @@ plot_training_curve(None, 'Mean episodic reward', log_zip, 'training_results.png
 
 plot_result("global_temperature",
             "temperature_result.png",
-            nego_off=gpu_nego_off_ts, # change it to cpu_nego_off_ts if using CPU
-            nego_on=gpu_nego_on_ts, 
+            nego_off=nego_off_ts, # change it to cpu_nego_off_ts if using CPU
+            nego_on=nego_on_ts, 
             k=0)
 
 tracker.stop()
