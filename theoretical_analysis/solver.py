24befd9f34b27bef39d1d65d4008d6ca226170fa
Edward Li <edwardli@andrew.cmu.edu>
2022-12-09 17:34:44 -0500

    Solver prototype

--- theoretical_analysis/solver.py
+++ theoretical_analysis/solver.py
@@ -9,3 +9,11 @@ Payoff = namedtuple("Payoff", ["a", "b"])
 class Node:
     actions: Dict[Action, Union["Node", Payoff]]
     is_leaf: bool
+
+class Solver:
+    def __init__(self, game_root: Node):
+        self.game_root = None
+
+    def solve(self):
+        # Returns: strategy (mapping from Node -> Action), equilibrium path (Action list)
+        raise NotImplementedError
