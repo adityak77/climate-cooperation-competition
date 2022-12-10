a0b56ecd1c53a24d60eeebecbf62ca5c5dca148d
Edward Li <edwardli@andrew.cmu.edu>
2022-12-10 01:36:40 -0500

    Finish SPE Solver
    This solver is untested, with a few caveats. Because solving
    imperfect-information extensive form games requires a solution concept like
    course-correlated equilibria (which might be ok, but would require CFR), or a
    sequential equilibrium (which I believe requires exponential-time worst case to
    solve), we just treat the toy climate game as a perfect information extensive
    form game, making the stage game a commitment game. This should be fine as long
    as the stage game remains a tragedy of the commons and not something like a
    Chicken-like game, as the pure strategy Nash equilibria are the same as the pure
    strategy Stackleberg equilibria in a ToC game.

--- theoretical_analysis/solver.py
+++ theoretical_analysis/solver.py
@@ -1,19 +1,41 @@
 from collections import namedtuple
 from dataclasses import dataclass
-from typing import Union, Dict
+from typing import Union, Dict, Hashable
 
-Action = namedtuple("Action", ["a", "b"])
 Payoff = namedtuple("Payoff", ["a", "b"])
 
 @dataclass
 class Node:
-    actions: Dict[Action, Union["Node", Payoff]]
-    is_leaf: bool
+    # [info] should be used to store things like n, m, and t
+    # for use later in move analysis
+    info: Hashable
+    player: int
+    actions: Dict[int, Union["Node", Payoff]]
 
 class Solver:
     def __init__(self, game_root: Node):
-        self.game_root = None
+        self.game_root = game_root
+        self.strategy = None
+
+    def _solve_subtree(self, subtree: Node):
+        if isinstance(subtree, Payoff):
+            return subtree, []
+
+        num_actions = len(subtree.actions)
+        subtree_sols = [(act, self._solve_subtree(subtree.actions[act])) for act in range(num_actions)]
+
+        # [max] returns the first encountered item in case of a tie. We want to break ties in favor of
+        # mitigation, so we reverse the subtree_sols
+        best_action, (payoff, path) = max(subtree_sols[::-1], key=lambda x: x[1][0][subtree.player])
+
+        self.strategy[subtree] = best_action
+
+        return payoff, [(subtree, best_action), ] + path
 
     def solve(self):
-        # Returns: strategy (mapping from Node -> Action), equilibrium path (Action list)
-        raise NotImplementedError
+        # Returns: strategy (mapping from Node -> Action), equilibrium path ((Node * Action) list)
+        self.strategy = {}
+        _, path = self._solve_subtree(self.game_root)
+
+        return self.strategy, path
+
